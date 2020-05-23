d57f08c39266ec22aeaa90bebd67ea6a1edf30b6
zww1990 <zww1990@foxmail.com>
2020-05-23 16:04:42 +0800

    Integer to String

--- backend/spring-webapp/src/main/java/com/amazing/controller/UserController.java
+++ backend/spring-webapp/src/main/java/com/amazing/controller/UserController.java
@@ -23,10 +23,10 @@ public class UserController {
 		UserModel model = new UserModel();
 		if (casEnabled) {
 			log.info("UserId = {}", principal.getName());
-			model.setUserId(Integer.parseInt(principal.getName()));
+			model.setUserId(principal.getName());
 			model.setUserName("攻城狮");
 		} else {
-			model.setUserId(666666);
+			model.setUserId("666666");
 			model.setUserName("超级管理员");
 		}
 		return model;
