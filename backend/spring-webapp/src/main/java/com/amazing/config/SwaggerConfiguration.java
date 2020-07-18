ecac67648eaab41a6110dff0188f9b63eb7a7008
zww1990 <zww1990@foxmail.com>
2020-07-18 15:05:32 +0800

    swagger 2.x to 3.x

---
+++ backend/spring-webapp/src/main/java/com/amazing/config/SwaggerConfiguration.java
@@ -0,0 +1,34 @@
+package com.amazing.config;
+
+import java.util.Collections;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableOpenApi
+@ConditionalOnWebApplication
+@EnableConfigurationProperties(SwaggerProperties.class)
+public class SwaggerConfiguration {
+	@Bean
+	public Docket api(SwaggerProperties props) {
+		return new Docket(DocumentationType.OAS_30).select()
+				.apis(RequestHandlerSelectors.basePackage(props.getBasePackage())).build().apiInfo(this.apiInfo(props));
+	}
+
+	private ApiInfo apiInfo(SwaggerProperties props) {
+		Contact contact = new Contact(props.getName(), props.getUrl(), props.getEmail());
+		return new ApiInfo(props.getTitle(), props.getDescription(), props.getVersion(), props.getTermsOfServiceUrl(),
+				contact, props.getLicense(), props.getLicenseUrl(), Collections.emptyList());
+	}
+
+}
---
+++ backend/spring-webapp/src/main/java/com/amazing/config/SwaggerProperties.java
@@ -0,0 +1,98 @@
+package com.amazing.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "springfox.swagger")
+public class SwaggerProperties {
+	private String title;
+	private String description;
+	private String version;
+	private String termsOfServiceUrl;
+	private String name;
+	private String url;
+	private String email;
+	private String license;
+	private String licenseUrl;
+	private String basePackage;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getTermsOfServiceUrl() {
+		return termsOfServiceUrl;
+	}
+
+	public void setTermsOfServiceUrl(String termsOfServiceUrl) {
+		this.termsOfServiceUrl = termsOfServiceUrl;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getLicense() {
+		return license;
+	}
+
+	public void setLicense(String license) {
+		this.license = license;
+	}
+
+	public String getLicenseUrl() {
+		return licenseUrl;
+	}
+
+	public void setLicenseUrl(String licenseUrl) {
+		this.licenseUrl = licenseUrl;
+	}
+
+	public String getBasePackage() {
+		return basePackage;
+	}
+
+	public void setBasePackage(String basePackage) {
+		this.basePackage = basePackage;
+	}
+
+}
